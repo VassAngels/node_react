00f4f8a5436c1bf4c7210a7167948e5e16279657
Vassilis Angelis <angelis.vassilis@gmail.com>
2019-06-22 23:07:11 +0100

    complete EditProfile component

--- react-front/src/user/EditProfile.js
+++ react-front/src/user/EditProfile.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react'
 import {Redirect}  from 'react-router-dom'
 import {isAuthenticated} from '../auth'
-import {read} from './apiUser'
+import {read,update} from './apiUser'
 
 
 class EditProfile extends Component {
+    ///////////////////////////////////////////////
     //Creating state
+    ///////////////////////////////////////////////
     constructor() {
         super()
         this.state = {
@@ -13,16 +15,20 @@ class EditProfile extends Component {
             name: "",
             email: "",
             password: "",
-            redirectToProfile: false 
+            redirectToProfile: false,
+            error: ''
         }
     }
-    
+    ///////////////////////////////////////////////
     //1st step. Grabbing the user id from the props
+    ///////////////////////////////////////////////
     componentDidMount(){
         const userId = this.props.match.params.userId
         this.init(userId)
     }
-    //2nd step. Get request to the backend to get user info to propulate values in the form
+    ///////////////////////////////////////////////
+    //2nd step. Make a Get request to the backend to get user info to prepopulate values in the form
+    ///////////////////////////////////////////////
     init = (userId) =>{
         const token = isAuthenticated().token;
         read(userId, token)
@@ -32,44 +38,38 @@ class EditProfile extends Component {
                 console.log("error")}
             else {this.setState({id:data._id, name: data.name, email: data.email})}
         })
-        console.log('1')
-        console.log(this.state.password)
-        console.log('2')
     }
-
-    //Passing,reflecting changes made in the form to the component state
+    ///////////////////////////////////////////////
+    //Passing/reflecting the changes made in the form to the component state
+    ///////////////////////////////////////////////
     handleChange =(type) => event =>{
         this.setState({[type]: event.target.value})
-
     }
-    //submit signup data to the back end method
+    ///////////////////////////////////////////////
+    //submit update data to the back end method////
+    ///////////////////////////////////////////////
     clickUpdate=(event)=>{
         event.preventDefault(); //prevent refreshing
         const {name,email, password}=this.state //getting the data from the state
         const user = //creating user object to be passed to the back end
         {   name: name,
             email: email,
-            password:password
-
+            password:password || undefined
         }
-        console.log('3')
-        console.log(user)
-        // signUp(user)
-        // .then(Data => {
-        //     if(Data.error) this.setState({error:Data.error})
-        //         else this.setState({
-        //             name:"",
-        //             password: "",
-        //             email: "",
-        //             error: "",
-        //             open: true
-        //         }
-        //         )
-        // })
-
+        const userId = this.props.match.params.userId//grab userID from router parameter
+        const token = isAuthenticated().token; //grab token
+        update(userId, token, user)//make the request to the backend for updating the user
+        .then(responseData => {
+            if(responseData.error) this.setState({error:responseData.error})
+                else this.setState({
+                    redirectToProfile: true
+                }
+                )
+        })
     }
-
-    //form to be rendered
+    ///////////////////////////////////////////////
+    //form to be rendered/////////////////////////
+    ///////////////////////////////////////////////
     UpdateForm =(name,email,password)=>( 
         <form>
             <div className ="form-group">
@@ -86,18 +86,22 @@ class EditProfile extends Component {
             </div>
             <button onClick={this.clickUpdate} className ="btn btn-raised btn-primary">Update</button>
         </form>)
-
+    ///////////////////////////////////////////////
+    ///////////////////////////////////////////////
     render(){
-        const {id, name, email,password, redirectToProfile} = this.state
+        const {id, name, email,password, redirectToProfile, error} = this.state
         if(redirectToProfile){
            return <Redirect to={`/user/${id}`}/>
         }
-        console.log('render')
-        console.log(password)
         return(
             <div className='container'>
                 <h2 className="my-5 mb-5">Edit Profile</h2>
+                 {/* conditional rendering */}
+                <div className="alert alert-danger" style={{ display: error ? "": "none"}}>
+                    {this.state.error}
+                </div>               
                 {this.UpdateForm(name,email,password)}
+
             </div>
         )
     }
